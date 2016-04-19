3 and 4 player games. Fixed negative wealth. Added softmax action selection

robber/q_learn.py
@@ -7,7 +7,7 @@ class Agent:
 
     agent_list = {}
 
-    def __init__(self, name, params=(.45, .9)):
+    def __init__(self, name, params=(.45, .8)):
         self.order = len(Agent.agent_list.values())
         self.str = name
         self.Q = {}
@@ -16,31 +16,55 @@ class Agent:
         self.alpha, self.gamma = params  # learning rate, memory
         Agent.agent_list[name] = self
 
+        self.static = False
+
     @classmethod
     def get_agents(cls):
         return Agent.agent_list.values()
 
+    def summary_actions(self):  # all state:action pairs w/ non-zero Q
+        kist = [{k: v for k, v in self.Q[key].iteritems() if v != 0} for key in self.Q.keys()]
+        return {k: v for k, v in dict(zip(self.Q.keys(), kist)).iteritems() if bool(v)}
+
     def take_action(self, state, valid_acts):
-        eps = np.random.rand(1)
-        if eps > 0.1:  # 90% chance
-            # np.random.choice(np.where(a == a.max())[0]) #non-working
+        # SOFT-MAX #
+        # -------- #
+        tau = 1.0
+        self.make_key(state, valid_acts)
+        try:
+            ex = np.exp(np.array([self.Q[state][tuple(i)] for i in valid_acts])/tau)
+        except KeyError:
+            print 'WHAT!!?'
+            raise
+        pmf = ex/np.sum(ex)
 
-            # This chooses the action associated with the max Q value
-            move = max(self.Q[state], key=self.Q[state].get)
+        # move = np.random.choice(valid_acts, size=1, p=pmf)
+        move = valid_acts[np.random.choice(valid_acts.shape[0], p=pmf)]
+        return move
 
-            # move=np.max([self.Q[state][i] for i in self.Q[state]]) #selcts wrong thing
-            return move
-        else:  # 20% chance
-            return valid_acts[np.random.choice(valid_acts.shape[0])]
+        # EPSILON-GREEDY #
+        # -------------- #
+        # eps = np.random.rand(1)
+        # if eps > 0.1:  # 90% chance
+        #     # np.random.choice(np.where(a == a.max())[0]) #non-working
+        #
+        #     # This chooses the action associated with the max Q value
+        #     move = max(self.Q[state], key=self.Q[state].get)
+        #
+        #     # move=np.max([self.Q[state][i] for i in self.Q[state]]) #selcts wrong thing
+        #     return move
+        # else:  # 20% chance
+        #     return valid_acts[np.random.choice(valid_acts.shape[0])]
 
     def make_key(self, state, valid_acts):
         # print self.Q
         # print type(state)
         if state not in self.Q:
             self.Q[state] = {}
-            for i in valid_acts:
-                # initialize with small random values
-                self.Q[state][tuple(i)] = np.random.uniform(low=-0.15, high=.15)
+        for i in valid_acts:
+            # initialize with small random values
+            # self.Q[state][tuple(i)] = np.random.uniform(low=-0.15, high=.15)
+            self.Q[state][tuple(i)] = 0.
 
     def update_Q(self, state, move, valids, *conds):
         win_check, lose_check = conds
@@ -50,23 +74,30 @@ class Agent:
         self.make_key(state, valids)
         self.make_key(old_state, valids)
 
+        if self.static:
+            return
+
         if win_check:
+            # print 'Player {} wins!'.format(self.str)
+            # print self.Q[old_state][move]
             self.games_played +=1
             self.wins += 1
-            reward = 10.
+            reward = 1.
             self.Q[old_state][move] = (self.Q[old_state][move] +
-                                          self.alpha * (reward -
+                                          self.alpha * (reward +
                                                    self.Q[old_state][move]))
+            # print self.Q[old_state][move]
         elif lose_check:
             self.games_played += 1
-            reward = -10.
+            reward = -1.
             # print state+move, move
             self.Q[old_state][move] = (self.Q[old_state][move] +
-                                          self.alpha * (reward -
-                                                   self.Q[old_state][move]))
+                                       self.alpha * (reward -
+                                                     self.Q[old_state][move]))
         else:
             self.Q[old_state][move] = (self.Q[old_state][move] +
-                                          self.alpha * (reward -
-                                                   self.gamma * self.Q[state][
-                                                       max(self.Q[state], key=self.Q[state].get)] -
-                                                   self.Q[old_state][move]))
+                                       self.alpha * (reward -
+                                                     self.gamma * self.Q[state][max(self.Q[state],
+                                                                                    key=self.Q[state].get)] -
+                                                     self.Q[old_state][move]))
+        return

robber/robbery.py
@@ -16,7 +16,7 @@ class Robbery:
 
 
     def loss(self, player_no):
-        conds = [i >= 10 for i in self.state]
+        conds = [i >= 3 for i in self.state]
         del conds[player_no]
         if any(conds):
             return True
@@ -24,7 +24,7 @@ class Robbery:
             return False
 
     def win(self, player_no):
-        if self.state[player_no] >= 10:
+        if self.state[player_no] >= 3:
             return True
         else:
             return False
@@ -37,7 +37,7 @@ class Robbery:
         # p = (np.array([1, 2, 3, 4, 5, 5, 4, 3, 2, 1]) / 36.).mean()
         p = 0.0834
         # har = np.array([np.random.binomial(i, .084) for i in self.civ_size()])
-        har = np.random.binomial(3, .084, size=4)
+        har = np.random.binomial(2, .084, size=self.num_p)
         if self.blocked is not None:
             har[self.blocked] = 0
             # print 'thief is blocking Player {0}'.format(self.blocked)
@@ -56,7 +56,12 @@ class Robbery:
             move[j] = 1
             move[i] = -1
             moves += [move]
-        return np.array(moves)
+        mask = np.logical_or(np.array(self.state) != 0, np.array(moves)[:, j] == 0)
+        mask[j] = True
+        # print np.min()
+        # print player_no
+        # print np.array(moves), self.state, mask
+        return np.array(moves)[mask]
 
     def take_turn(self, player):
 
@@ -75,14 +80,17 @@ class Robbery:
             self.state[self.state < 0] = 0
             self.state = tuple(self.state)
 
+            # player.update_Q(self.state, move, valids,
+            #                 self.win(player.order),
+            #                 self.loss(player.order))
             for agent in self.playlist:
                 offset = agent.order - player.order
                 rmove = tuple(np.roll(move, offset))
                 rstate = tuple(np.roll(self.state, offset))
                 rvalids = np.roll(valids, offset, axis=1)
                 agent.update_Q(rstate, rmove, rvalids,
-                               self.win(agent.order),
-                               self.loss(agent.order))
+                               self.win(player.order),
+                               self.loss(player.order))
 
     def one_round(self):
         for i in self.playlist: